a395f4346558c486193c5965b23007d0947b4320
Adam Luz <amlutz160@gmail.com>
2020-03-03 17:28:09 -0600

    stackNavigator init

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const IndexScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Here2</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginLeft: 15
+  },
+})
+
+export default IndexScreen;

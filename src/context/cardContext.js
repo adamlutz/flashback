402f576cdee9e394f0c7c12a2d39b6326a4492c3
Adam Luz <amlutz160@gmail.com>
2020-03-04 11:03:10 -0600

    useState to useReducer

--- src/context/cardContext.js
+++ src/context/cardContext.js
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const CardContext = React.createContext();
+
+const cardReducer = (state, action) => {
+  switch (action.type) {
+    case 'add_card':
+      return [...state, { title: `my card # ${state.length + 1}` }]
+    default:
+      return state;
+  }
+}
+
 
 // this is a named export
 // chilcren is basically 'App'
 export const CardProvider = ({ children }) => {
 
-  const [cards, setCards] = useState([]);
+  // cards == state
+  const [cards, dispatch] = useReducer(cardReducer, []);
+
+  // rather than creating multiple methods for each crud
+  // operation, use reducers instead!
   const addCard = () => {
-    setCards([...cards, { title: `my card # ${cards.length + 1}` }]);
+    dispatch({ type: 'add_card', payload: 'title' })
   };
 
   return <CardContext.Provider value={{ data: cards, addCard }}>

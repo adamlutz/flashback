0045c209a6142c28b396eda7d2833f1e300ae253
Adam Luz <amlutz160@gmail.com>
2020-03-04 16:04:04 -0600

    basic styles

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,18 +1,22 @@
 import React, { useContext } from 'react';
 import { Button, View, Text, StyleSheet, FlatList } from 'react-native';
 import { Context as CardContext } from '../context/CardContext'
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
   const { state, addCard } = useContext(CardContext);
 
   return (
-    <View style={styles.container}>
+    <View>
       <Button title='Add card' onPress={addCard} />
       <FlatList
         data={state}
         keyExtractor={(card) => card.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>
+          return <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Feather style={styles.icon} name='trash' />
+          </View>
         }}
       />
     </View>
@@ -20,8 +24,19 @@ const IndexScreen = () => {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginLeft: 15
+  row: {
+    borderTopWidth: 1,
+    borderColor: 'gray',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10
+  },
+  title: {
+    fontSize: 18
+  },
+  icon: {
+    fontSize: 20
   },
 })
 

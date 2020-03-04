a294137c37ab4ec914a0efb65890fb612191554a
Adam Luz <amlutz160@gmail.com>
2020-03-04 16:25:56 -0600

    show screen updates

--- src/screens/ShowScreen.js
+++ src/screens/ShowScreen.js
@@ -2,11 +2,14 @@ import React, { useContext } from 'react';
 import { Button, View, Text, StyleSheet, FlatList } from 'react-native';
 import { Context as CardContext } from '../context/CardContext'
 
-const ShowScreen = () => {
+const ShowScreen = ({ navigation }) => {
+
+  const { state } = useContext(CardContext);
+  const card = state.find((card) => card.id === navigation.getParam('id'));
 
   return (
     <View>
-      <Text>Show screen</Text>
+      <Text>{card.title}</Text>
     </View>
   )
 }
